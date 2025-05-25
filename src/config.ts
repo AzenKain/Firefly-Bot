b54f0fc1d75707a5ba0c0b804a6c37a9d372be53
AzenKain <doduykhanh344@gmail.com>
2025-05-25 22:38:26 +0700

    fix deploy

--- src/config.ts
+++ src/config.ts
@@ -3,16 +3,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { DISCORD_TOKEN, DISCORD_CLIENT_ID, DISCORD_GUILD_ID, DISCORD_PREFIX } = process.env;
+let { DISCORD_TOKEN, DISCORD_CLIENT_ID, DISCORD_PREFIX } = process.env;
 
-if (!DISCORD_TOKEN || !DISCORD_CLIENT_ID || !DISCORD_GUILD_ID || !DISCORD_PREFIX) {
+if (!DISCORD_TOKEN || !DISCORD_CLIENT_ID ) {
   throw new Error("Missing environment variables");
 }
-
+if (!DISCORD_PREFIX) {
+  DISCORD_PREFIX = "f?";
+}
 export const config = {
   DISCORD_TOKEN,
   DISCORD_CLIENT_ID,
-  DISCORD_GUILD_ID,
   DISCORD_PREFIX,
 };
 
